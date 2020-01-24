b1a55736baa818974b77079f1522148f567aa144
KinKam Zhao <Kamzh88@KinKams-Air.fios-router.home>
2020-01-23 23:50:07 -0500

    change moment ajax to inside its bottom

--- public/js/adminView.js
+++ public/js/adminView.js
@@ -6,88 +6,88 @@ $(document).ready(function () {
     $.ajax("/api/menu", {
         type: "GET"
     }).then(function (data) {
-        $.ajax("/api/moment", {
-            type: "GET"
-        }).then(function (time) {
 
-            var itemize = [];
-            var timeData = time.time;
-            var dateData = time.date;
-            var itemID = [];
-            var subTotal;
-            var priceArray = [];
-            var categoryElem = $('#category-div');
-            // console.log(data);
+        var itemize = [];
+        var itemID = [];
+        var subTotal;
+        var priceArray = [];
+        var categoryElem = $('#category-div');
+        // console.log(data);
 
-            //append category onto the page
-            var category = ["Chicken", "Beef", "Appetizers", "Vegetables", "Seafood"];
-            var len = category.length;
+        //append category onto the page
+        var category = ["Chicken", "Beef", "Appetizers", "Vegetables", "Seafood"];
+        var len = category.length;
+        for (var i = 0; i < len; i++) {
+            var new_elem = `<button class='categorybtn' data-id=${data.menu} data-category=${category[i]}>${category[i]}</button>`
+            categoryElem.append(new_elem);
+        }
+
+        //categorybtn data
+        $(document).on("click", ".categorybtn", function (event) {
+            var itemsDiv = $('#items-div');
+            var categorybtn = $(this).data("category");
+            var len = data.menu.length;
+            itemsDiv.empty();
             for (var i = 0; i < len; i++) {
-                var new_elem = `<button class='categorybtn' data-id=${data.menu} data-category=${category[i]}>${category[i]}</button>`
-                categoryElem.append(new_elem);
+                if (categorybtn === data.menu[i].category) {
+                    var itemName = data.menu[i].item_name
+                    var new_elem = `<button class='itembtn' data-id=${data.menu[i].id} data-item=${itemName}>${itemName}</button>`
+                    itemsDiv.append(new_elem);
+                };
             }
+        })
 
-            //categorybtn data
-            $(document).on("click", ".categorybtn", function (event) {
-                var itemsDiv = $('#items-div');
-                var categorybtn = $(this).data("category");
-                var len = data.menu.length;
-                itemsDiv.empty();
-                for (var i = 0; i < len; i++) {
-                    if (categorybtn === data.menu[i].category) {
-                        var itemName = data.menu[i].item_name
-                        var new_elem = `<button class='itembtn' data-id=${data.menu[i].id} data-item=${itemName}>${itemName}</button>`
-                        itemsDiv.append(new_elem);
-                    };
-                }
-            })
-
-            //itemID is the ID of the on click function
-            $(document).on("click", ".itembtn", function (event) {
-                var id = $(this).data("id");
-                var orderDiv = $('#order-div');
-                var cartDiv = $('.cart-body');
-                var len = data.menu.length;
-                for (var i = 0; i < len; i++) {
-                    if (id === data.menu[i].id) {
-                        var itemName = data.menu[i].item_name;
-                        var itemPrice = data.menu[i].price;
-                        var new_elem = `
+        //itemID is the ID of the on click function
+        $(document).on("click", ".itembtn", function (event) {
+            var id = $(this).data("id");
+            var orderDiv = $('#order-div');
+            var cartDiv = $('.cart-body');
+            var len = data.menu.length;
+            for (var i = 0; i < len; i++) {
+                if (id === data.menu[i].id) {
+                    var itemName = data.menu[i].item_name;
+                    var itemPrice = data.menu[i].price;
+                    var new_elem = `
                         <button type="button" class="onClickItem" data-id=${id}><span class="item-name" data-id=${id}>${itemName}</span><span class="boldPriceCSS">$${itemPrice}</span></button><br>
                             `;
-                        orderDiv.append(new_elem);
-                        cartDiv.append(new_elem);
-                        priceArray.push(parseFloat(itemPrice));
-                        subTotal = priceArray.reduce((a, b) => a + b, 0)
-                        totals(subTotal);
-                        itemID.push(id);
-                    };
-                }
-            })
-            // var deleteIDArray = [];
-            // $(document).on("click", ".onClickItem", function (event) {
-            //     $(this).toggleClass("changeState");
-            //     var deleteID = $(this).data("id");
-            //     deleteIDArray.push(deleteID);
+                    orderDiv.append(new_elem);
+                    cartDiv.append(new_elem);
+                    priceArray.push(parseFloat(itemPrice));
+                    subTotal = priceArray.reduce((a, b) => a + b, 0)
+                    totals(subTotal);
+                    itemID.push(id);
+                };
+            }
+        })
+        // var deleteIDArray = [];
+        // $(document).on("click", ".onClickItem", function (event) {
+        //     $(this).toggleClass("changeState");
+        //     var deleteID = $(this).data("id");
+        //     deleteIDArray.push(deleteID);
 
-            // })
+        // })
 
-            // $(document).on("click", ".orderReceiptDeleteButton", function (event) {
-            //     console.log(deleteIDArray);
-            //     var id = deleteIDArray
-            //     for (var i = 0; i < id; i++) {
-            //         $.ajax('/api/orders', id[i], {
-            //             type: "DELETE"
-            //         }).then(function () {
-            //             // console.log("Item has been deleted!");
-            //             // location.reload();
-            //         })
-            //     }
-            // })
+        // $(document).on("click", ".orderReceiptDeleteButton", function (event) {
+        //     console.log(deleteIDArray);
+        //     var id = deleteIDArray
+        //     for (var i = 0; i < id; i++) {
+        //         $.ajax('/api/orders', id[i], {
+        //             type: "DELETE"
+        //         }).then(function () {
+        //             // console.log("Item has been deleted!");
+        //             // location.reload();
+        //         })
+        //     }
+        // })
 
 
-            $(".form-orderlist").on("submit", function (event) {
-                event.preventDefault();
+        $(".form-orderlist").on("submit", function (event) {
+            event.preventDefault();
+            $.ajax("/api/moment", {
+                type: "GET"
+            }).then(function (time) {
+                var timeData = time.time;
+                var dateData = time.date;
                 var len = data.menu.length;
                 for (var i = 0; i < len; i++) {
                     for (var j = 0; j < len; j++) {
@@ -115,15 +115,16 @@ $(document).ready(function () {
                     // console.log(result);
                 })
             })
+        })
 
-            //Edit Menu Button on main page
-            $(document).on("click", "#menu-changes", function (event) {
-                // console.log(data.menu);
-                var menuElem = $(".modal-body");
-                var items = data.menu;
-                var len = data.menu.length;
-                for (var i = 0; i < len; i++) {
-                    var new_elem = `
+        //Edit Menu Button on main page
+        $(document).on("click", "#menu-changes", function (event) {
+            // console.log(data.menu);
+            var menuElem = $(".modal-body");
+            var items = data.menu;
+            var len = data.menu.length;
+            for (var i = 0; i < len; i++) {
+                var new_elem = `
                 <ul class="edit-heading"> ${items[i].id}. ${items[i].item_name}   $${items[i].price}
                 <button class='delete-item' data-id=${items[i].id}><i class="fas fa-window-close"></i></button>
                 <h4 class="panel-title"><a data-toggle="collapse" href="#collapse${i}">Edit</a></h4>
@@ -154,33 +155,33 @@ $(document).ready(function () {
                   </div>
                 </div></ul>`
 
-                    menuElem.append(new_elem);
-                };
+                menuElem.append(new_elem);
+            };
 
-                //Save button in edit button modal
-                $(".edit-form").on("submit", function (event) {
-                    event.preventDefault();
-                    var id = $(this).data("id");
-                    var itemName = $(event.target).closest("item-name").context[0].value;
-                    var categoryName = $(event.target).closest("item-name").context[1].value;
-                    var itemPrice = $(event.target).closest("item-name").context[2].value;
-                    var editItem = {
-                        item_name: itemName,
-                        category: categoryName,
-                        selected: 0,
-                        price: itemPrice
-                    };
-                    // console.log(editItem);
-                    $.ajax("/api/menu/" + id, {
-                        type: "PUT",
-                        data: JSON.stringify(editItem),
-                        dataType: 'json',
-                        contentType: 'application/json'
-                    }).then(function (data) {
-                        // console.log("menu item changed", id);
-                        location.reload();
-                        // console.log(data);
-                        $(event.target).closest(".edit-heading").html(`
+            //Save button in edit button modal
+            $(".edit-form").on("submit", function (event) {
+                event.preventDefault();
+                var id = $(this).data("id");
+                var itemName = $(event.target).closest("item-name").context[0].value;
+                var categoryName = $(event.target).closest("item-name").context[1].value;
+                var itemPrice = $(event.target).closest("item-name").context[2].value;
+                var editItem = {
+                    item_name: itemName,
+                    category: categoryName,
+                    selected: 0,
+                    price: itemPrice
+                };
+                // console.log(editItem);
+                $.ajax("/api/menu/" + id, {
+                    type: "PUT",
+                    data: JSON.stringify(editItem),
+                    dataType: 'json',
+                    contentType: 'application/json'
+                }).then(function (data) {
+                    // console.log("menu item changed", id);
+                    location.reload();
+                    // console.log(data);
+                    $(event.target).closest(".edit-heading").html(`
                     <ul class="edit-heading"> ${id}. ${data.item_name}   $${data.price}
                     <button class='delete-item' data-id=${id}>DELETE</button>
                     <h4 class="panel-title"><a data-toggle="collapse" href="#collapse${i}">Edit</a></h4>
@@ -210,73 +211,73 @@ $(document).ready(function () {
                   </form>
                       </div>
                     </div></ul>`);
-                    })
                 })
-            });
-            $(document).on("click", "#order-list", function (event) {
-                $.ajax("/api/orders", {
-                    type: "GET"
-                }).then(function (orders) {
+            })
+        });
 
+        $(document).on("click", "#order-list", function (event) {
+            $.ajax("/api/orders", {
+                type: "GET"
+            }).then(function (orders) {
 
-                    var dateArray = [];
-                    var subtotalArray = [];
-                    var taxArray = [];
-                    var totalArray = [];
-                    // var itemNameArray = [];
-                    var order_div = $(".body3");
+                var dateArray = [];
+                var subtotalArray = [];
+                var taxArray = [];
+                var totalArray = [];
+                // var itemNameArray = [];
+                var order_div = $(".body3");
 
-                    for (var i = 0; i < orders.orders.length; i++) {
-                        // console.log("Ticket Number: " + orders.orders[i].id);
-                        var ticketNumber = orders.orders[i].id;
-                        var date = orders.orders[i].date;
-                        var time = orders.orders[i].time;
-                        var ticketNumber_elem = `
+                for (var i = 0; i < orders.orders.length; i++) {
+                    // console.log("Ticket Number: " + orders.orders[i].id);
+                    var ticketNumber = orders.orders[i].id;
+                    var date = orders.orders[i].date;
+                    var time = orders.orders[i].time;
+                    var ticketNumber_elem = `
                             <button data-dismiss="modal" id="modal3" data-ticket=${ticketNumber} class= "order">
                             ${ticketNumber}. ${date} ${time}</button><br>`;
-                        order_div.append(ticketNumber_elem)
+                    order_div.append(ticketNumber_elem)
+                }
+
+                $(document).on("click", `.order`, function (event) {
+                    var orderScreen = $(".orderGrid");
+                    orderScreen.empty();
+                    var ticket = $(this).data("ticket");
+                    var orderDiv = $('#order-div');
+
+                    for (var j = 0; j < data.menu.length; j++) {
+                        var dataID = data.menu[j].id
+                        // console.log("dataID:" + dataID);
                     }
 
-                    $(document).on("click", `.order`, function (event) {
-                        var orderScreen = $(".orderGrid");
-                        orderScreen.empty();
-                        var ticket = $(this).data("ticket");
-                        var orderDiv = $('#order-div');
-
-                        for (var j = 0; j < data.menu.length; j++) {
-                            var dataID = data.menu[j].id
-                            // console.log("dataID:" + dataID);
+                    for (var i = 0; i < orders.orders.length; i++) {
+                        var dataTicketNumber = orders.orders[i].id;
+                        if (ticket === dataTicketNumber) {
+                            var itemize_id = orders.orders[i].itemize_id.split(',');
+                            var subtotal = orders.orders[i].subtotal;
+                            var taxes = orders.orders[i].taxes;
+                            var total = orders.orders[i].total;
                         }
+                    }
 
-                        for (var i = 0; i < orders.orders.length; i++) {
-                            var dataTicketNumber = orders.orders[i].id;
-                            if (ticket === dataTicketNumber) {
-                                var itemize_id = orders.orders[i].itemize_id.split(',');
-                                var subtotal = orders.orders[i].subtotal;
-                                var taxes = orders.orders[i].taxes;
-                                var total = orders.orders[i].total;
-                            }
-                        }
-
-                        console.log((itemize_id));
-                        var itemNameArray = [];
-                        var itemPriceArray = [];
-                        for (var k = 0; k < itemize_id.length; k++) {
-                            for (var l = 0; l < data.menu.length; l++) {
-                                var dataID = data.menu[l].id
-                                if (itemize_id[k] == dataID) {
-                                    var itemName = data.menu[l].item_name;
-                                    var itemPrice = data.menu[l].price
-                                    console.log(data.menu[l]);
-                                    itemNameArray.push(itemName);
-                                    itemPriceArray.push(itemPrice);
-                                }
-
+                    console.log((itemize_id));
+                    var itemNameArray = [];
+                    var itemPriceArray = [];
+                    for (var k = 0; k < itemize_id.length; k++) {
+                        for (var l = 0; l < data.menu.length; l++) {
+                            var dataID = data.menu[l].id
+                            if (itemize_id[k] == dataID) {
+                                var itemName = data.menu[l].item_name;
+                                var itemPrice = data.menu[l].price
+                                console.log(data.menu[l]);
+                                itemNameArray.push(itemName);
+                                itemPriceArray.push(itemPrice);
                             }
 
                         }
 
-                        var new_elem = `
+                    }
+
+                    var new_elem = `
                         <form class="form-orderlist">
                         <h1 id="order-heading">Ticket # ${ticket}</h1><br>
                         <div id="order-div">
@@ -299,20 +300,20 @@ $(document).ready(function () {
                             <button type="submit" class="btn btn-primary submit">Quit</button>
                         </div>
                         </form>`;
-                        orderScreen.append(new_elem);
+                    orderScreen.append(new_elem);
 
-                        for (var k = 0; k < itemize_id.length; k++) {
-                            var itemNameDiv = $(".item-name")
-                            var item_elem = `
+                    for (var k = 0; k < itemize_id.length; k++) {
+                        var itemNameDiv = $(".item-name")
+                        var item_elem = `
                             ${itemNameArray[k]} <span class="boldCSS">$${itemPriceArray[k]}</span><br>
                         `
-                            itemNameDiv.append(item_elem);
-                        }
-                    });
-                })
+                        itemNameDiv.append(item_elem);
+                    }
+                });
             })
-        });
-    })
+        })
+    });
+
 
     function totals(subTotal) {
         var totalDiv = $("#total");
@@ -389,5 +390,6 @@ $(document).ready(function () {
 
     setInterval(updateTime, 1000);
     updateTime();
+
 })
 
